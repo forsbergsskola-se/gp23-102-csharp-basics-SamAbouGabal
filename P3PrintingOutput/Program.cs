58bfdb8cb8375153b06d168a2ab75fad71e5d140
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-04 16:14:26 +0200

    Deleted old P2CodeExection and created a new
    . Also added P3PrintingOutput

---
+++ P3PrintingOutput/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, Output!");
+Console.WriteLine("Hello, World");
+Console.WriteLine("Welcome to 102 Csharp basics!");
+Console.WriteLine("It's easy to print output...");
+Console.WriteLine("...to the console!");
