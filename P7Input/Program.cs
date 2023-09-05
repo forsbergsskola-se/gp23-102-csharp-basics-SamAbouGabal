95a4d93d6c93260a0f368bb38067bdc49d38eb3d
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-05 22:23:38 +0200

    Deleted P8Conversion and added a new P8conversion
    Old one bugged out so made a new one

--- P7Input/Program.cs
+++ P7Input/Program.cs
@@ -1,3 +1,18 @@
 ﻿Console.WriteLine("What's your name?");
 Console.Read();
 Console.WriteLine("Hello, Sam!");
+
+
+
+
+
+using System.Globalization;
+
+float number = 5.6f;
+Console.WriteLine("Give me a number. Preferably 5.6");
+var input  = Console.ReadLine();
+Console.WriteLine(number);
+double fractionalNumber = 5;
+Console.WriteLine(fractionalNumber);
+fractionalNumber = Convert.ToDouble(input, CultureInfo.InvariantCulture);
+Console.WriteLine (fractionalNumber);
