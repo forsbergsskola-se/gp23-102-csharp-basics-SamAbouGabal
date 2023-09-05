4d4124a5e55d1ba5b1e22d8f99a84b0e29d2d976
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-05 09:51:46 +0200

    Added P4Comments

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello, World");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome to 102 Csharp basics!");
 Console.WriteLine("It's easy to print output...");
 Console.WriteLine("...to the console!");
