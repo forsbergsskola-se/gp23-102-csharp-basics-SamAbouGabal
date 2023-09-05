46c11312a509937d6015cdb954e40020e84b1dd5
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-05 14:40:03 +0200

    Added dialogue

--- P7Input/Program.cs
+++ P7Input/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("What's your name?");
+string? answer = Console.Read();
+Console.WriteLine("Aah, your name was ");
+Console.WriteLine(answer);
