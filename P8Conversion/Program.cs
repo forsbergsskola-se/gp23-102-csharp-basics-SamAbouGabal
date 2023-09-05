0a0034bf8b29a055b731d813cb9487be9645e999
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-05 22:24:34 +0200

    Added the same code from old P8

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Globalization;
+
+float number = 5.6f;
+Console.WriteLine("Give me a number. Preferably 5.6");
+var input  = Console.ReadLine();
+Console.WriteLine(number);
+double fractionalNumber = 5;
+Console.WriteLine(fractionalNumber);
+fractionalNumber = Convert.ToDouble(input, CultureInfo.InvariantCulture);
+Console.WriteLine (fractionalNumber);
