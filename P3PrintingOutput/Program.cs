8cc99817d8e308f37cc07be08840036382f7c1ff
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-05 09:19:35 +0200

    Update Program.cs

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,5 +1,4 @@
-﻿Console.WriteLine("Hello, Output!");
-Console.WriteLine("Hello, World");
+﻿Console.WriteLine("Hello, World");
 Console.WriteLine("Welcome to 102 Csharp basics!");
 Console.WriteLine("It's easy to print output...");
 Console.WriteLine("...to the console!");
