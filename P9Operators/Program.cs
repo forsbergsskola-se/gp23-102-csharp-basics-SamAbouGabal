dfe4e8f3b9c25bf652eaa844b9be0624fef77137
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-06 12:05:01 +0200

    P9 Exercise 1 done

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿/* Exercise 1
+
+Console.WriteLine("welcome to km/h to m/h converter");
+Console.WriteLine("Please enter a value in km/h");
+string userInput = Console.ReadLine();
+float userNumber = float.Parse(userInput);
+float x = userNumber * 1000 / 60 / 60;
+Console.WriteLine($"That would be {x} m/s");
+*/
+
