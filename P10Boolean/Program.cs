6f6aa71eaa5e953c949a17f519c00254b4c6ebf9
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-07 13:33:48 +0200

    Finished first P10Boolean exercise

--- P10Boolean/Program.cs
+++ P10Boolean/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Type in your age");
+string userInput = Console.ReadLine();
+int age = Convert.ToInt32(userInput);
+bool isChild = age <= 12;
+Console.WriteLine($"You are a child:{isChild}");
+bool isTeenager = age >= 13 && age <= 19;
+Console.WriteLine($"You are a teenager:{isTeenager}");
+bool isGrownup = age >= 20;
+Console.WriteLine($"You are a grown-up:{isGrownup}");
