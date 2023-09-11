11a1a440634cf529a3fea3f902ded04a894cd738
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-11 10:40:57 +0200

    Finished more exercises

--- GP23 exercise discord/Program.cs
+++ GP23 exercise discord/Program.cs
@@ -1,8 +1,8 @@
-﻿/* Exercise 1
+﻿/*
 Console.WriteLine("Hello, Give me a Km/h, and i will output m/s");
 string userInput = Console.ReadLine();
 float number = float.Parse(userInput);
-double x = number * 0.27778;
+double x = number / 60 / 60 * 1000;
 Console.WriteLine($"m/s is:{x}");
 */
 
@@ -18,7 +18,7 @@ Console.WriteLine("Give me the first number to divide:");
 int numberX = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("through: ");
 int numberY = Convert.ToInt32(Console.ReadLine());
-float answer = (float)numberX / (float)numberY;
+float answer = (float)numberX / numberY;               //only one needs to be (float) doesn't matter wich one
 Console.WriteLine($"Your answer is: {answer}");
 */
 
@@ -35,7 +35,7 @@ Console.WriteLine($"the remainder of the two numbers are: {mod}");
 Console.WriteLine("Give me the radius of a circle");
 float userInput = float.Parse(Console.ReadLine());
 double answer = userInput * userInput * 3.14159;
-Console.WriteLine($"The are of the circle is {answer}");
+Console.WriteLine($"The area of the circle is {answer}");
 */
 
 /*
@@ -43,5 +43,14 @@ Console.WriteLine("Give me a number for me to change it's negation");
 int userInput = int.Parse(Console.ReadLine());
 int negationNumber = -userInput;
 Console.WriteLine($"Here's the negation: {negationNumber}");
+*/
+
+/*
+Console.WriteLine("input your weight in kg");
+float userInputkg = float.Parse(Console.ReadLine());
+Console.WriteLine("Input your height in cm");
+float userInputcm = float.Parse(Console.ReadLine());
+float bodyMass = userInputkg / (userInputcm*userInputcm /100f /100f);
+Console.WriteLine($"Your BMI is {bodyMass}");
 */
 
