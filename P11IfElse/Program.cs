7d9328f47aff5d624bbeaf041dc5731275b384b6
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-12 09:22:08 +0200

    First exercise in P11

--- P11IfElse/Program.cs
+++ P11IfElse/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿/* Exercise 1
+Console.WriteLine("What's your age?");
+string ageInput = Console.ReadLine();
+int age = int.Parse(ageInput);
+
+if (age < 13)
+{
+    Console.WriteLine("You are a child.");
+}
+
+else if (age >= 13 && age <= 19)
+{
+    Console.WriteLine("You are a teen.");
+}
+
+else if (age > 19)
+{
+    Console.WriteLine("You are an adult.");
+}
+*/
