55031d34a8c9cd3165ea5d391dd2826db4e5a543
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-14 13:43:58 +0200

    NIM game complete

--- NIM/Program.cs
+++ NIM/Program.cs
@@ -1,12 +1,12 @@
 ﻿int matches = 24;
 bool userTurn = true;
-bool aiChoise = (!userTurn);
 Console.WriteLine(@"Hello and welcome to the game of NIM!
 The game starts with 24 matches. You can remove 1,2 or 3 matches per turn.
 The one who draws the last match loose");
-while (matches > 0);
-Console.WriteLine("\n Matches left: ", new string('|',matches));
-    if(userTurn);
+while (matches > 0)
+{
+    Console.WriteLine($"\n Matches left: " + new string('|', matches));
+    if (userTurn)
     {
         int userChoise;
         Console.WriteLine("Your turn. Remove 1-3 matches");
@@ -14,16 +14,19 @@ Console.WriteLine("\n Matches left: ", new string('|',matches));
         {
             Console.WriteLine("Invalid input. Input a number between 1-3");
         }
-        
+
         matches -= userChoise;
         Console.WriteLine($"You removed {userChoise} matches.");
     }
-    if (aiChoise)
+    else
     {
         int aiChoice = Math.Min(matches, new Random().Next(1, 4));
         matches -= aiChoice;
         Console.WriteLine($"The game removed {aiChoice} matches.");
     }
+userTurn =!userTurn;
+
+}
 if (userTurn)
 {
     Console.WriteLine("Congratulations you win!");
