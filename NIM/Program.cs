4c0331234fdfe0d74d062b469090b1ac42fe361b
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-09-14 14:24:05 +0200

    Removed While statement aswell as added counter to the right of the matches

--- NIM/Program.cs
+++ NIM/Program.cs
@@ -5,14 +5,16 @@ The game starts with 24 matches. You can remove 1,2 or 3 matches per turn.
 The one who draws the last match loose");
 while (matches > 0)
 {
-    Console.WriteLine($"\n Matches left: " + new string('|', matches));
+    Console.WriteLine($"\n Matches left: {new string('|', matches)} ({matches})");
     if (userTurn)
     {
         int userChoise;
         Console.WriteLine("Your turn. Remove 1-3 matches");
-        while (!int.TryParse(Console.ReadLine(), out userChoise) || userChoise < 1 || userChoise > 3)
+        UserInputAgain:
+        if (!int.TryParse(Console.ReadLine(), out userChoise) || userChoise < 1 || userChoise > 3)
         {
             Console.WriteLine("Invalid input. Input a number between 1-3");
+            goto UserInputAgain;
         }
 
         matches -= userChoise;
@@ -20,13 +22,14 @@ while (matches > 0)
     }
     else
     {
-        int aiChoice = Math.Min(matches, new Random().Next(1, 4));
+        int aiChoice = Math.Min(matches, Random.Shared.Next(1, 4));
         matches -= aiChoice;
         Console.WriteLine($"The game removed {aiChoice} matches.");
     }
-userTurn =!userTurn;
 
+    userTurn = !userTurn;
 }
+
 if (userTurn)
 {
     Console.WriteLine("Congratulations you win!");
